83a192eed3f20ba629ae057d5f504f3f89042bfb
Summer498 <h262sawamura@i.softbank.jp>
2022-07-07 13:37:17 +0900

    first commit

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
-npm
-start
-git
-init
-git
-add
-.
-git
-commit
--m
-first commit
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/Summer498/react-training.git
+git push origin master
